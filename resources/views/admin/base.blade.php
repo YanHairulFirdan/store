2d12f5255e8604fc4773e715085472fbc18defe5
yanhf <yanhairulfirdan@gmail.com>
2021-06-07 14:13:25 +0700

    fixing layouts for user and admin

--- resources/views/admin/base.blade.php
+++ resources/views/admin/base.blade.php
@@ -1,6 +1,21 @@
-@include('partials.topnavs')
-@include('partials.sidebar')
+@extends('layouts.skeleton')
 
-@yield('content')
+@section('app')
+    <div class="main-wrapper">
+        <div class="navbar-bg"></div>
+        <nav class="navbar navbar-expand-lg main-navbar">
+            @include('partials.topnav')
+        </nav>
+        <div class="main-sidebar">
+            @include('partials.sidebar')
+        </div>
 
-@include('partials.footer')
+        <!-- Main Content -->
+        <div class="main-content">
+            @yield('content')
+        </div>
+        <footer class="main-footer">
+            @include('partials.footer')
+        </footer>
+    </div>
+@endsection
